b0bdd91bca60944cb1e148cba6a99ddd2ab9e90d
Shiva Amoria <shiva.amoria@walkingtree.tech>
2023-04-26 17:49:30 +0530

    changes in persister and yml file of ilms

--- core-services/egov-persister/src/main/java/org/egov/infra/persist/web/contract/Mapping.java
+++ core-services/egov-persister/src/main/java/org/egov/infra/persist/web/contract/Mapping.java
@@ -10,29 +10,47 @@ import java.util.List;
 @Setter
 @Getter
 @ToString
-public class Mapping   {
-	
-  @JsonProperty("version")
-  private String version = null;
+public class Mapping {
 
-  @JsonProperty("name")
-  private String name = null;
+    @JsonProperty("version")
+    private String version = null;
 
-  @JsonProperty("fromTopic")
-  private String fromTopic = null;
-  
-  @JsonProperty("description")
-  private String description = null;
+    @JsonProperty("name")
+    private String name = null;
 
-  @JsonProperty("isTransaction")
-  private Boolean isTransaction = true;
+    @JsonProperty("fromTopic")
+    private String fromTopic = null;
 
-  @JsonProperty("isBatch")
-  private Boolean isBatch = false;
+    @JsonProperty("description")
+    private String description = null;
 
-  @JsonProperty("queryMaps")
-  private List<QueryMap> queryMaps = null;
+    @JsonProperty("isTransaction")
+    private Boolean isTransaction = true;
 
- 
+    @JsonProperty("isBatch")
+    private Boolean isBatch = false;
+
+    @JsonProperty("queryMaps")
+    private List<QueryMap> queryMaps = null;
+
+    @JsonProperty("module")
+    private String module = null;
+
+    @JsonProperty("objecIdJsonPath")
+    private String objecIdJsonPath = null;
+
+    @JsonProperty("tenantIdJsonPath")
+    private String tenantIdJsonPath = null;
+
+    @JsonProperty("transactionCodeJsonPath")
+    private String transactionCodeJsonPath = null;
+
+    @JsonProperty("isAuditEnabled")
+    private Boolean isAuditEnabled = null;
+
+    @JsonProperty("auditAttributeBasePath")
+    private String auditAttributeBasePath = null;
+
+
 }
 
